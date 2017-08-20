cea4cfb495bf598632315c4a8e77727f9b697aab
tsarachan <tes353@nyu.edu>
2017-08-20 15:12:08 -0400

    Basic scene; PlayManager
    Added a manager script for the game’s core logic, and a basic scene for
    it to exist in.

---
+++ FlankingBlocks/Assets/Scripts/Architecture/PlayManager.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * 
+ * This script handles the top-level logic of actual gameplay: setting up the board, checking for the players' actions, etc.
+ * 
+ */
+
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour {
+
+
+	/// <summary>
+	/// Initialize variables and objects
+	/// </summary>
+	private void Start(){
+		Services.EventSys = new EventSystem();
+	}
+
+
+	/// <summary>
+	/// The core update loop of the game. For ease of handling things like pausing, this is the game's single update loop;
+	/// no object is allowed to have its own.
+	/// </summary>
+	private void Update(){
+
+	}
+}
