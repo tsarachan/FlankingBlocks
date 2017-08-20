f7f74fb4961a494dcfa0d5cf66601b86d8aa1333
tsarachan <tes353@nyu.edu>
2017-08-20 15:01:00 -0400

    Added service locator

---
+++ FlankingBlocks/Assets/Scripts/Architecture/Services.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 
+ * This service locator provides one-stop access to esssential architectural elements of the code.
+ * 
+ */
+
+using UnityEngine;
+
+public abstract class Services {
+
+	//event system
+	private static EventSystem eventSys;
+	public static EventSystem EventSys {
+		get { 
+			Debug.Assert(eventSys != null);
+			return eventSys;
+		}
+
+		set { eventSys = value; }
+	}
+}
